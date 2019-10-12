ea1545c82a690bd4d39016aa8647bb1227920fb5
psgivens <psgivens11@yahoo.com>
2019-10-11 19:38:11 -0700

    Moved webapp to its own repo.

--- webapp/src/basic/reducers/counterReducers.ts
+++
@@ -1,9 +0,0 @@
-import { CounterEvent } from '../actions/CounterSaga'
-
-export function counterReducers(state:number=0, action: CounterEvent): number {
-    switch (action.type) {
-        case "COUNTER_INCREMENT_SUCCESS": return state + 1
-        default: return state
-    }
-}
-
